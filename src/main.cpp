88bc1dcb01bf205eec00afa9ce3453dae305e591
Frem <kor7781@gmail.com>
2023-06-24 21:44:21 +0300

    add boost library

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
-
+#include "include/frem.h"
+#include <boost/array.hpp>
 int main()
 {
-	std::cout<<"hello cmake"<<std::endl;
+
+	std::cout << "hello cmake" << std::endl;
+	Frem::Meow();
+	std::cin.get();
+
 	return 0;
 }
